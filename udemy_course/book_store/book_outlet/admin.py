6ba27c392de40c35804bcebc5da0faafccf7b9e8
hyorrr <129085830+hyorrr@users.noreply.github.com>
2025-07-23 01:05:48 +0900

    Relationships

--- udemy_course/book_store/book_outlet/admin.py
+++ udemy_course/book_store/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address, Country
 
 # Register your models here.
 
@@ -8,8 +8,14 @@ class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)} # 사용하려면 editable=False 지우기
     list_filter = ("author", "rating",)
     list_display = ("title", "author",)
+    
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name",)
 
     
 
 # admin.site.register(Book)
 admin.site.register(Book,BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address)
+admin.site.register(Country)
